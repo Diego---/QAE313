3a1fec27b4621dbc0e04bf88199ebe579c96eafe
Diego Olvera <omega_717@hotmail.com>
2025-06-16 12:53:08 +0200

    clean up fidelity measurement and make it more modular

--- qae/circuits/fidelity_measurement.py
+++ qae/circuits/fidelity_measurement.py
@@ -5,9 +5,7 @@ import numpy as np
 from typing import Callable
 
 from qiskit import QuantumCircuit
-from qiskit.primitives import BackendEstimator
-from qiskit.primitives.primitive_job import PrimitiveJob
-from qiskit.quantum_info import Statevector, partial_trace
+from qiskit.quantum_info import Statevector, DensityMatrix
 from qiskit_experiments.library import StateTomography
 from qiskit.circuit import Parameter
 from qiskit.providers import Backend
@@ -16,7 +14,7 @@ from qiskit_aer.noise import NoiseModel
 from qiskit.quantum_info import state_fidelity
 
 from qae.circuits.circuit_building import build_aux_circs
-from qae.circuits.circuit_constants import zero, init_states_complete, circ_labels
+from qae.circuits.circuit_constants import init_states_complete, circ_labels
 
 Num = float | int | np.number
 logger = logging.getLogger(__name__)
@@ -24,16 +22,18 @@ logger = logging.getLogger(__name__)
 def evaluate_final_state_fidelity(
         param_dict: dict[Parameter, float], 
         backend: Backend, 
-        ansatz: QuantumCircuit | None = None, 
+        ansatz: QuantumCircuit | None = None,
+        encoding_circ: QuantumCircuit | None = None,
+        measurement_circ: QuantumCircuit | None = None,
         init_states: list[QuantumCircuit] | None = None,
         final_rotations: list[QuantumCircuit] | None = None,
+        error_list: list[int] | list[QuantumCircuit] | None = None,
+        initializer_dictionaries: list[dict] | None = None,
         num_shots: int = 200,
         mini_batch: int | None = None,
         noise_model: NoiseModel | None = None,
         use_tomo: bool = False,
-        do_continous_evaluation: bool = False,
-        use_pseudo_fidelity: bool = True,
-        skip_compilation: bool = False,
+        expected_final_state: Statevector | DensityMatrix | QuantumCircuit | None = None,
         **kwargs):
     """
     Calculate the fidelity of each circuit with a given ansatz.
@@ -46,10 +46,21 @@ def evaluate_final_state_fidelity(
         The backend to execute the circuits on.
     ansatz : QuantumCircuit 
         The QuantumCircuit object representing the ansatz circuit.
+    encoding_circ: QuantumCircuit, optional
+        The type of circuit used for encoding/decoding the initial states into the codespace. Defaults
+        to 3 qubit repetition code encoding.
+    measurement_circ: QuantumCircuit, optional
+        The measurements to be performed provided as a QuantumCircuit. Defaults to measuring
+        qubit 0 into classical bit 0.
     init_states : list[QuantumCircuit] 
         A list of QuantumCircuit objects representing initial states.
     final_rotations : list[QuantumCircuit] 
         A list of QuantumCircuit objects representing the final rotations required by the initial states.
+    error_list : list[int], list[QuantumCircuit], optional
+        List of errors to be used. Defaults to single qubit flips in each qubit as a list of QuantumCircuits.
+    initializer_dictionaries : list[dict], optional
+        List of dictionaries to assign to a completely parametrized circuit representation of the QAE problem.
+        The ansaztz is interpreted as the fully parametrized representation.
     num_shots : int 
         The number of shots for each circuit execution.
     mini_batch : int, optional 
@@ -58,12 +69,9 @@ def evaluate_final_state_fidelity(
         Noise model to be used in the simulation.
     use_tomo : bool
         Whether to do state tomography on the resulting one qubit state or just use the measurement counts.
-    do_continous_evaluation : bool
-        Whether every time the fidelity of the resulting measurement statistics are close to the ideal simulation.
-    use_pseudo_fidelity : bool
-        Whether one should calculate a pseudo-fidelity only out of the measurement statistics. Defaults to True.
-    skip_compilation : bool
-        Whether to skip compilation.
+    expected_final_state : Statevector | DensityMatrix | QuantumCircuit, optional
+        The expected final state against which the final fidelity is measured. Defaults to the single qubit
+        |0> state.
 
     Returns
     ----------
@@ -81,28 +89,30 @@ def evaluate_final_state_fidelity(
     The fidelity is calculated using state fidelity, where the resulting state is compared to the target state |0⟩.
     """
 
-    if not use_pseudo_fidelity:
-        raise NotImplementedError("This functionality has not been implemented.")
-
     fids = []
     jobs = []
+    
+    # If initializer_dictionaries is set, aux_circs will be a list of dictionaries with
+    # the different initialization parameter maps (parameters which have to be set to get initial
+    # states and other circuit settings) joined with the parameter_map to evaluate the final state
+    # fidelity. Otherwise, aux circs are QuantumCircuits.
     aux_circs = build_aux_circs(ansatz=ansatz, 
                                 init_states=init_states, 
-                                final_rotations=final_rotations, 
+                                final_rotations=final_rotations,
+                                measurement_circ=measurement_circ,
+                                initializer_dictionaries=initializer_dictionaries,
+                                encoding_circ=encoding_circ,
+                                error_list=error_list,
                                 param_dict=param_dict,
-                                use_resets=False)
+                                )
     
-    if do_continous_evaluation:
-        aux_circs_eval = aux_circs.copy
-        ideal_states: list[Statevector] = []
-    
+    # Set the circuits that will be used to evaluate the fidelity
     uci = kwargs.get('used_circs_indices')
-    
+
     if not (uci is None):
-        used_circs: list[QuantumCircuit] = [aux_circs[i] for i in uci]
+        used_circs: list[QuantumCircuit | dict] = [aux_circs[i] for i in uci]
         logger.info(f"Used circuits: {', '.join(circ_labels[i] for i in uci)}.")
-        if do_continous_evaluation:
-            used_circs_eval: list[QuantumCircuit] = [aux_circs_eval[i] for i in uci]
+        used_circ_labels = [circ_labels[i] for i in uci]
     else:
         if mini_batch:
             num_circs_per_group = min(mini_batch, 4)
@@ -113,56 +123,61 @@ def evaluate_final_state_fidelity(
                 inds.append(i+8)
             used_circs = [aux_circs[i] for i in inds]
             logger.info(f"Used circuits: {', '.join(circ_labels[i] for i in inds)}")
-            if do_continous_evaluation:
-                used_circs_eval = [aux_circs_eval[i] for i in inds]
+            used_circ_labels = [circ_labels[i] for i in inds]
         else:
             used_circs: list[QuantumCircuit] = aux_circs.copy()
-            if do_continous_evaluation:
-                used_circs_eval: list[QuantumCircuit] = aux_circs_eval.copy()
+            used_circ_labels = circ_labels.copy()
 
     logger.info(f"{len(used_circs)} circuits were used to calculate the averaged fidelity.")
     if noise_model:
         logger.info("A noise model was used.")
     if use_tomo:
-        logger.info("Performing state tomography on resulting qubit state.")
+        if expected_final_state is None:
+            final_state = Statevector.from_label('0')
+        elif isinstance(expected_final_state, QuantumCircuit):
+            final_state = Statevector(expected_final_state)
+        elif not isinstance(expected_final_state, (Statevector, DensityMatrix)):
+            raise TypeError(f"Expected Statevector, Density Matrix, or QuantumCircuit object, not {type(expected_final_state)}.")
+        else:
+            final_state = expected_final_state.copy()
+        logger.info("Performing state tomography on resulting qubit state against provided final state.")
+        
+    # Set up the jobs to be ran
     for i, circ in enumerate(used_circs):
-
-        if do_continous_evaluation:
-            circ_no_measurements = used_circs_eval[i].remove_final_measurements(inplace = False)
-            ideal_state = Statevector(circ_no_measurements)
-            ideal_state = partial_trace(ideal_state, [1,2])
-            ideal_states += [ideal_state]
-
         if use_tomo:
             tomography = StateTomography(
                 circuit = circ.remove_final_measurements(inplace = False),
                 backend = backend,
                 measurement_indices = [0]
                 )
-            jobs += [tomography.run(backend=backend)]
+            jobs += [tomography.run(backend=backend, job_name = f'Circuit_{used_circ_labels[i]}')]
         else:
             if noise_model:
-                job : Job = backend.run([circ], shots = num_shots, noise_model = noise_model, job_name = f'Circuit_{i}')
+                job : Job = backend.run(
+                    [circ], shots = num_shots, noise_model = noise_model, job_name = f'Circuit_{used_circ_labels[i]}'
+                    )
             else:
-                if backend.name in ['red_trap_backend', 'umz_simulator_backend'] and skip_compilation:
+                # The ansatz is the full parametrized circuit and circ = aux_circ[i] is the parameter dictionary 
+                # for circuit i with the parameters given by param_dict.
+                if backend.name in ['red_trap_backend', 'umz_simulator_backend'] and not initializer_dictionaries is None:
                     job : Job = backend.run(
-                        [circ],
+                        [ansatz],
                         shots = num_shots,
-                        job_name = f'Circuit_{i}',
-                        use_rz_phase_tracking = False
+                        job_name = f'Circuit_{used_circ_labels[i]}',
+                        execution_settings = {'parameters': circ}
                     )
                 else:
-                    job : Job = backend.run([circ], shots = num_shots, job_name = f'Circuit_{i}')
+                    # aux_circs are already the circuits that need to be run in the not fully parametrized implementation. 
+                    job : Job = backend.run([circ], shots = num_shots, job_name = f'Circuit_{used_circ_labels[i]}')
             jobs += [job]
 
+    # Run the circuits, either by calculating the probability of getting 0 
+    # or by doing final state tomography against expexted_final_state
     for i, job in enumerate(jobs):
         if use_tomo:
             resulting_density_matrix = job.analysis_results("state").value
-            fid = state_fidelity(resulting_density_matrix, zero)
+            fid = state_fidelity(resulting_density_matrix, final_state)
             fids += [fid]
-
-            if do_continous_evaluation:
-                evaluation_fid = state_fidelity(resulting_density_matrix, ideal_states[i])
         else:
             counts =  job.result().get_counts()
             logger.info(f"Resulting counts are: {counts}")
@@ -181,16 +196,9 @@ def evaluate_final_state_fidelity(
             # that produces the measurement statistics (when measuring sigma_Z) of the 0 state 
             # is the 0 state, so comparing using only the frequencies (populations) should be 
             # sufficient.
-            pseudo_state = np.dot([np.array([1,0]), np.array([0,1])], np.sqrt(frequencies))
-            # The target state is always the zero state.
-            fid = state_fidelity(pseudo_state, zero)
-            fids += [fid]
-
-            if do_continous_evaluation:
-                evaluation_fid = state_fidelity(pseudo_state, ideal_states[i])
-
-        if do_continous_evaluation:
-            logger.info(f"The fidelity between the simulated state and the executed statistics is: {evaluation_fid}.")
+            # The target state is always the zero state. This is effectively just the probability
+            # of measuring 0, approximated by the first element of frequencies.
+            fids += [frequencies[0]]
 
     return fids
 
@@ -198,8 +206,7 @@ def create_av_fidelity(backend: Backend,
                        mini_batch: int | None = None, 
                        noise_model: NoiseModel | None = None,
                        use_tomo: bool = False,
-                       do_continous_evaluation: bool = False,
-                       skip_compilation: bool = False):
+                       ) -> Callable:
     """
     Create a function to calculate the average fidelity of circuits with a given ansatz.
 
@@ -209,7 +216,7 @@ def create_av_fidelity(backend: Backend,
         The backend to execute the circuits on.
     mini_batch : int
         The size of mini-batch to use for evaluating circuits.
-    noise_model : NoiseModel
+    noise_model : NoiseModel, optional
         Optional NoiseModel to be used in the simulation results.
     use_tomo : bool
         Whether to do state tomography on the resulting one qubit state or just use the measurement counts.
@@ -231,80 +238,46 @@ def create_av_fidelity(backend: Backend,
     is computed as the negative average of the fidelity values obtained from `evaluate_final_state_fidelity`.
     """
     def averaged_fidelity(
-        ansatz: QuantumCircuit, 
-        parameters: list[Num] | dict[Parameter, Num | Parameter],
+        ansatz: QuantumCircuit,
+        parameters: list[Num] | dict[str | Parameter, Num | Parameter],
         num_shots: int, 
         init_states: list[QuantumCircuit] | None, 
-        final_rotations: list[QuantumCircuit] | None, 
+        final_rotations: list[QuantumCircuit] | None,
+        error_list: list[int] | list[QuantumCircuit] | None,
+        initializer_dictionaries: list[dict] | None,
+        encoding_circ: QuantumCircuit | None,
+        measurement_circ: QuantumCircuit | None,
         **kwargs
         ) -> Num:
         
         if isinstance(parameters, dict):
             param_dict = parameters
-        else:
+        elif initializer_dictionaries is None:
             param_dict = {ansatz_parameter : parameters[i] for i, ansatz_parameter in enumerate(ansatz.parameters)}
-
-        if init_states is None:
-            init_states = init_states_complete.copy()
+        else:
+            actual_ansatz_parameters = [param.name for param in ansatz.parameters if param.name not in initializer_dictionaries[0]]
+            param_dict = {ansatz_parameter : parameters[i] for i, ansatz_parameter in enumerate(actual_ansatz_parameters)}
 
         uci = kwargs.get('used_circs_indices')
 
-        fids = evaluate_final_state_fidelity(param_dict, backend, ansatz, init_states, final_rotations,  
-                                                num_shots, mini_batch, used_circs_indices = uci, 
-                                                noise_model = noise_model,  use_tomo = use_tomo,
-                                                do_continous_evaluation = do_continous_evaluation,
-                                                skip_compilation = skip_compilation)
+        fids = evaluate_final_state_fidelity(
+            param_dict, 
+            backend, 
+            ansatz,
+            encoding_circ,
+            measurement_circ,
+            init_states, 
+            final_rotations,
+            error_list,
+            initializer_dictionaries,
+            num_shots, 
+            mini_batch, 
+            noise_model=noise_model,  
+            use_tomo=use_tomo,
+            used_circs_indices=uci, 
+            )
 
-        return -np.average(fids)
+        average = np.average(fids)
+        return -average
     
     return averaged_fidelity
-
-def create_stochastic_av_fidelity(backend: Backend, 
-                                  noise_model: NoiseModel | None = None,
-                                  use_tomo: bool = False,
-                                  do_continous_evaluation: bool = False,
-                                  skip_compilation: bool = False):
-    """
-    Create a function to calculate the average fidelity of circuits with a given ansatz.
-
-    Parameters
-    ----------
-    backend : Backend 
-        The backend to execute the circuits on.
-    noise_model : NoiseModel
-        Optional NoiseModel to be used in the simulation results.
-    use_tomo : bool
-        Whether to do state tomography on the resulting one qubit state or just use the measurement counts.
-    do_continous_evaluation : bool
-        Whether every time the fidelity of the resulting measurement statistics are close to the ideal simulation.
-    skip_compilation : bool
-        Whether to skip compilation.
-
-    Returns
-    ----------
-    function
-        A function that calculates the average fidelity of circuits with a given ansatz.
-
-    Description
-    -----------
-    This function creates a closure that generates another function `averaged_fidelity`. The `averaged_fidelity` function 
-    calculates the average fidelity of circuits with a given ansatz. It utilizes the `evaluate_final_state_fidelity` function to 
-    evaluate fidelity for each circuit, using the provided ansatz, parameters, initial states, and backend. The average fidelity 
-    is computed as the negative average of the fidelity values obtained from `evaluate_final_state_fidelity`.
-    """
-    def averaged_fidelity(ansatz: QuantumCircuit, parameters: list[Num], num_shots: int, init_states: list[QuantumCircuit] | None, final_rotations: list[QuantumCircuit] | None, **kwargs):
-        param_dict = {ansatz_parameter : parameters[i] for i, ansatz_parameter in enumerate(ansatz.parameters)}
-
-        if init_states is None:
-            init_states = init_states_complete.copy()
-
-        used_circs_indices = kwargs['used_circs_indices'] 
-        fids = evaluate_final_state_fidelity(param_dict, backend, ansatz, init_states, final_rotations, num_shots, 
-                                             used_circs_indices = used_circs_indices, noise_model = noise_model,
-                                             use_tomo = use_tomo, do_continous_evaluation = do_continous_evaluation,
-                                             skip_compilation = skip_compilation)
-            
-        return -np.average(fids)
-    
-    return averaged_fidelity
-
